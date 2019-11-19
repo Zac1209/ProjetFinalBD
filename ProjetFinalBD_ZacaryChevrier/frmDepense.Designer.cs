ce24c0192edeb39a69fdee4c0da1cda29b99a7cf
Zac1209 <zac123.yo@live.ca>
2019-11-19 18:11:45 -0500

    Dépenses en cours

---
+++ ProjetFinalBD_ZacaryChevrier/frmDepense.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace ProjetFinalBD_ZacaryChevrier
+{
+    partial class frmDepense
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.abonnementsDataGridView = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.abonnementsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.tbMontant = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbTypeService = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbRemarque = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnAjouter = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.abonnementsDataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.abonnementsBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbMontant)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // abonnementsDataGridView
+            // 
+            this.abonnementsDataGridView.AllowUserToAddRows = false;
+            this.abonnementsDataGridView.AllowUserToDeleteRows = false;
+            this.abonnementsDataGridView.AutoGenerateColumns = false;
+            this.abonnementsDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.abonnementsDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn3,
+            this.dataGridViewTextBoxColumn4});
+            this.abonnementsDataGridView.DataSource = this.abonnementsBindingSource;
+            this.abonnementsDataGridView.Location = new System.Drawing.Point(142, 56);
+            this.abonnementsDataGridView.Name = "abonnementsDataGridView";
+            this.abonnementsDataGridView.ReadOnly = true;
+            this.abonnementsDataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.abonnementsDataGridView.Size = new System.Drawing.Size(355, 127);
+            this.abonnementsDataGridView.TabIndex = 1;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "Id";
+            this.dataGridViewTextBoxColumn1.HeaderText = "Id";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.DataPropertyName = "Nom";
+            this.dataGridViewTextBoxColumn3.HeaderText = "Nom";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            this.dataGridViewTextBoxColumn3.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.DataPropertyName = "Prenom";
+            this.dataGridViewTextBoxColumn4.HeaderText = "Prenom";
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            this.dataGridViewTextBoxColumn4.ReadOnly = true;
+            // 
+            // abonnementsBindingSource
+            // 
+            this.abonnementsBindingSource.DataSource = typeof(ProjetFinalBD_ZacaryChevrier.Abonnements);
+            // 
+            // tbMontant
+            // 
+            this.tbMontant.Location = new System.Drawing.Point(142, 210);
+            this.tbMontant.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.tbMontant.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.tbMontant.Name = "tbMontant";
+            this.tbMontant.Size = new System.Drawing.Size(59, 20);
+            this.tbMontant.TabIndex = 2;
+            this.tbMontant.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Client";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(54, 212);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Montant";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(203, 213);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "$";
+            // 
+            // cbTypeService
+            // 
+            this.cbTypeService.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbTypeService.FormattingEnabled = true;
+            this.cbTypeService.Location = new System.Drawing.Point(142, 250);
+            this.cbTypeService.Name = "cbTypeService";
+            this.cbTypeService.Size = new System.Drawing.Size(121, 21);
+            this.cbTypeService.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 253);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Type de service";
+            // 
+            // tbRemarque
+            // 
+            this.tbRemarque.Location = new System.Drawing.Point(142, 293);
+            this.tbRemarque.Multiline = true;
+            this.tbRemarque.Name = "tbRemarque";
+            this.tbRemarque.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tbRemarque.Size = new System.Drawing.Size(121, 67);
+            this.tbRemarque.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(44, 314);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Remarque";
+            // 
+            // btnAjouter
+            // 
+            this.btnAjouter.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAjouter.Location = new System.Drawing.Point(142, 389);
+            this.btnAjouter.Name = "btnAjouter";
+            this.btnAjouter.Size = new System.Drawing.Size(105, 44);
+            this.btnAjouter.TabIndex = 10;
+            this.btnAjouter.Text = "Ajouter";
+            this.btnAjouter.UseVisualStyleBackColor = true;
+            this.btnAjouter.Click += new System.EventHandler(this.btnAjouter_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(199, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(161, 39);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Dépense";
+            // 
+            // frmDepense
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(538, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnAjouter);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tbRemarque);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cbTypeService);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbMontant);
+            this.Controls.Add(this.abonnementsDataGridView);
+            this.Name = "frmDepense";
+            this.Text = "frmDepense";
+            ((System.ComponentModel.ISupportInitialize)(this.abonnementsDataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.abonnementsBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbMontant)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.BindingSource abonnementsBindingSource;
+        private System.Windows.Forms.DataGridView abonnementsDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+        private System.Windows.Forms.NumericUpDown tbMontant;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbTypeService;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbRemarque;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnAjouter;
+        private System.Windows.Forms.Label label6;
+    }
+}
---
+++ ProjetFinalBD_ZacaryChevrier/frmDepense.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetFinalBD_ZacaryChevrier
+{
+    public partial class frmDepense : Form
+    {
+        DataClasses1DataContext monDataContext = new DataClasses1DataContext();
+        int serviceEmployeNo = 0;
+
+        public frmDepense()
+        {
+            InitializeComponent();
+            List<Abonnements> lstAboPrincipal = new List<Abonnements>();
+            var abo = from unAbo in monDataContext.Abonnements
+                      select unAbo;
+            foreach (Abonnements unAbo in abo)
+            {
+                String resultString = Regex.Match(unAbo.Id, @"\d+").Value;
+                int idEnChiffreNew = Int32.Parse(resultString);
+                String lettreDefinitionAbonnement = unAbo.Id.Substring(unAbo.Id.IndexOf(idEnChiffreNew.ToString()) + idEnChiffreNew.ToString().Length, 1);
+                if (lettreDefinitionAbonnement.ToUpper().Equals("P"))
+                {                    
+                    lstAboPrincipal.Add(unAbo);
+                }
+            }
+            abonnementsBindingSource.DataSource = lstAboPrincipal;
+
+            //Remplir liste service
+            Dictionary<int, String> dicTypeService = new Dictionary<int, String>();
+            dicTypeService.Add(5, "Magasin Pro Shop");
+            dicTypeService.Add(6, "Restaurant");
+            dicTypeService.Add(7, "Leçon de golf");
+            cbTypeService.DataSource = new BindingSource(dicTypeService, null);
+            cbTypeService.DisplayMember = "Value";
+            cbTypeService.ValueMember = "Key";
+
+            //Set automatiquement le type de service si applicable
+            if(frmMenu.userConnect.NoTypeEmploye >= 5)
+            {
+                cbTypeService.SelectedValue = frmMenu.userConnect.NoTypeEmploye;
+                cbTypeService.Enabled = false;
+
+                //Ajouter le service pour l'employé s'il n'existe pas
+                var service = (from unService in monDataContext.Services
+                              where unService.TypeService == cbTypeService.SelectedText && unService.NoEmploye == frmMenu.userConnect.No
+                              select unService).FirstOrDefault();
+                if(service == null)
+                {
+                    int noService = (from unService in monDataContext.Services
+                                     orderby unService.No descending
+                                     select (int)unService.No).FirstOrDefault() + 1;
+                    serviceEmployeNo = noService;
+                    Services serviceToAdd = new Services
+                    {
+                        No = noService,
+                        TypeService = cbTypeService.SelectedText,
+                        NoEmploye = frmMenu.userConnect.No
+                    };
+                    monDataContext.Services.InsertOnSubmit(serviceToAdd);
+                }
+                else
+                {
+                    serviceEmployeNo = service.No;
+                }
+            }
+        }
+
+        private void btnAjouter_Click(object sender, EventArgs e)
+        {
+            int noDepense = (from unDep in monDataContext.Depenses
+                             orderby unDep.No descending
+                             select (int)unDep.No).FirstOrDefault() + 1;
+            Depenses depense = new Depenses
+            {
+                No = noDepense,
+                IdAbonnement = abonnementsDataGridView.SelectedRows[0].Cells[0].Value.ToString(),
+                DateDepense = DateTime.Now,
+                Montant = tbMontant.Value,
+                NoService = serviceEmployeNo,
+                Remarque = tbRemarque.Text.Trim()
+            };
+            monDataContext.Depenses.InsertOnSubmit(depense);
+            try
+            {
+                monDataContext.SubmitChanges();
+                MessageBox.Show("Succès!", "Succès", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Close();
+            }
+            catch (Exception e1)
+            {
+                MessageBox.Show("Erreur", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Console.WriteLine(e1);
+            }
+        }
+    }
+}
